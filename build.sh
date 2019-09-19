c37139e6adf9ad0f69171eb34c7890add3c77cb4
Stefan Spiss <stefan.spiss@student.uibk.ac.at>
2019-09-19 12:34:26 +0200

    Added Pangolin as Thirdparty library.

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j8
 
 cd ../../g2o
 
@@ -13,7 +14,16 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j8
+
+cd ../../Pangolin
+
+echo "Configuring and building Thirdparty/Pangolin ..."
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j8
 
 cd ../../../
 
@@ -28,4 +38,4 @@ echo "Configuring and building ORB_SLAM2 ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j8
