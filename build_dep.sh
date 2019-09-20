1c0360ce4420d6384214ace60b4976bc1f141c39
Stefan Spiss <stefan.spiss@student.uibk.ac.at>
2019-09-20 16:54:46 +0200

    Added #!/bin/bash to build.sh script and added script to just build dependencies.

--- build_dep.sh
+++ build_dep.sh
@@ -23,10 +23,3 @@ echo "Uncompress vocabulary ..."
 cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
-
-echo "Configuring and building ORB_SLAM2 ..."
-
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j8
