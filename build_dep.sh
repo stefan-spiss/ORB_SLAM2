fa83dcda5ef736597918525747f27046331aab74
Stefan Spiss <stefan.spiss@student.uibk.ac.at>
2019-09-20 17:14:00 +0200

    Updated build_dep.sh script to take the path to the project root directory as input argument. Added command to call this script from cmake.

--- build_dep.sh
+++ build_dep.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
+
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
-cd Thirdparty/DBoW2
+cd $1/Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j8
-
-cd ../../g2o
 
 echo "Configuring and building Thirdparty/g2o ..."
 
+cd $1/Thirdparty/g2o
+
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j8
 
-cd ../../../
-
 echo "Uncompress vocabulary ..."
 
-cd Vocabulary
+cd $1/Vocabulary
 tar -xf ORBvoc.txt.tar.gz
-cd ..
